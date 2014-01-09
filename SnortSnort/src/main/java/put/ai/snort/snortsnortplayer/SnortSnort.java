7f440c05c7c6713461a8f0fd820ec3faf00a170d
MiS27 <michal.slusarczyk@student.put.poznan.pl>
2014-01-10 00:48:41 +0100

    alfaSnortBetaSnort-v.alfa

--- SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/SnortSnort.java
+++ SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/SnortSnort.java
@@ -18,12 +18,35 @@ public class SnortSnort extends Player {
     public String getName() {
         return "Aleksander Wdowiński 106525,\n"
         		+ "Michał Ślusarczyk 106643 (Gracz typu Snort-Snort. Tak rodzi się legenda.)";
-    }
-
+    }/*
     @Override
     public Move nextMove(Board b) {
         List<Move> moves = b.getMovesFor(getColor());
         int max = - b.getSize();
+        Move best = null;
+        for(Move move : moves) {
+                b.doMove(move);
+                int heur = heuristic(b, getColor());
+                if (max<heur) {
+                        max = heur;
+                        best = move;
+                }
+                
+                b.undoMove(move);
+        }
+        return best;
+    }*/
+    @Override
+    public Move nextMove(Board b) {
+    	int depth = 4;
+        List<Move> moves = b.getMovesFor(getColor());
+        if (moves.size()<30) 
+        	depth = 5;
+        if (moves.size()<20) 
+        	depth = 6;
+        if (moves.size()<10) 
+        	depth = 7;
+        int max = - b.getSize()-1;
         Move best = null;
         int alfa = - b.getSize();
         int beta = b.getSize();
@@ -41,21 +64,22 @@ public class SnortSnort extends Player {
     }
 
     public int alphabeta(Board b, int depth, int alfa, int beta, Color color){
+    	//if(true) return 0;
         if (depth == 0) {
             return heuristic(b, color);
         }
-        else if (b.getMovesFor(color).size()) {
-            return -64;
+        else if (b.getMovesFor(color).size() == 0) {
+            return -b.getSize();
         }
         if (color == getColor()) {
             for (Move move: b.getMovesFor(color)) {
                 b.doMove(move);
                 Move last_move = move;
                 alfa = Math.max(alfa, alphabeta(b, depth - 1, alfa, beta, getOpponent(color)));
+                b.undoMove(move);
                 if (beta <= alfa){
                     break;
                 }
-                b.undoMove(move);
             }
             return alfa;
         }
@@ -63,11 +87,11 @@ public class SnortSnort extends Player {
             for (Move move: b.getMovesFor(color)) {
                 b.doMove(move);
                 Move last_move = move;
-                beta = Math.min(beta, -alphabeta(b, depth - 1, alfa, beta, getOpponent(color)));
+                beta = Math.min(beta, alphabeta(b, depth - 1, alfa, beta, getOpponent(color)));
+                b.undoMove(move);
                 if (beta <= alfa){
                     break;
                 }
-                b.undoMove(move);
             }
             return beta;
         }
