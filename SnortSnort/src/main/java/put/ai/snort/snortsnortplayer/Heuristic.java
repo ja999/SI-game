488195fd215665992dfbdb5aba96f4b1be1c631e
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-01-09 22:33:31 +0100

    Adding heuristic assessment class

---
+++ SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/Heuristic.java
@@ -0,0 +1,18 @@
+package put.ai.snort.nastyplayer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import put.ai.snort.game.Board;
+import put.ai.snort.game.Move;
+import put.ai.snort.game.Player;
+
+public class Heuristic{
+	public Int Assess() {
+		return 1;
+	}
+}
