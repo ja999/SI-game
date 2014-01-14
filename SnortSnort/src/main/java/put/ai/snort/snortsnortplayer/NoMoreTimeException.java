f59b10770ca173ba5ce8b2316034b1c7a4f9f072
MiS27 <michal.slusarczyk@student.put.poznan.pl>
2014-01-14 04:17:31 +0100

    timeTime

---
+++ SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/NoMoreTimeException.java
@@ -0,0 +1,31 @@
+package put.ai.snort.snortsnortplayer;
+
+public class NoMoreTimeException extends Exception {
+
+	public NoMoreTimeException() {
+		super();
+		// TODO Automatycznie generowany szkielet konstruktora
+	}
+
+	public NoMoreTimeException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Automatycznie generowany szkielet konstruktora
+	}
+
+	public NoMoreTimeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Automatycznie generowany szkielet konstruktora
+	}
+
+	public NoMoreTimeException(String message) {
+		super(message);
+		// TODO Automatycznie generowany szkielet konstruktora
+	}
+
+	public NoMoreTimeException(Throwable cause) {
+		super(cause);
+		// TODO Automatycznie generowany szkielet konstruktora
+	}
+
+}
--- SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/SnortSnort.java
+++ SnortSnort/src/main/java/put/ai/snort/snortsnortplayer/SnortSnort.java
@@ -20,95 +20,89 @@ public class SnortSnort extends Player {
     public String getName() {
         return "Aleksander Wdowiński 106525,\n"
         		+ "Michał Ślusarczyk 106643 (Gracz typu Snort-Snort. Tak rodzi się legenda.)";
-    }/*
-    @Override
-    public Move nextMove(Board b) {
-        List<Move> moves = b.getMovesFor(getColor());
-        int max = - boardSize;
-        Move best = null;
-        for(Move move : moves) {
-                b.doMove(move);
-                int heur = heuristic(b, getColor());
-                if (max<heur) {
-                        max = heur;
-                        best = move;
-                }
-                
-                b.undoMove(move);
-        }
-        return best;
-    }*/
+    }
     int boardSize;
+    long startTime;
+    double late;
+    
     @Override
     public Move nextMove(Board b) {
-    	int depth = 4;
+    	startTime = System.currentTimeMillis();
+    	long appStartTime = System.nanoTime();
+    	int depth;
+    	int heur = 0;
+    	long appTime=0;
         List<Move> moves = b.getMovesFor(getColor());
+        if (getTime()>=200) {
+        	heur = heuristic(b,getColor());
+	    	b.doMove(moves.get(moves.size()/2));
+	    	b.undoMove(moves.get(moves.size()/2));
+	    	b.doMove(moves.get(moves.size()-1));
+	    	heuristic(b,getColor());
+	    	b.undoMove(moves.get(moves.size()-1));
+	    	appTime=(System.nanoTime()-appStartTime)/2;
+	        depth = (int) Math.floor(3.5*Math.log((getTime()-100)*1000000/appTime)/Math.log(moves.size()*(moves.size()-heur)));
+        }
+        else
+        	depth =1;
+        
+        if(late*4>1)
+        	depth--;
+        System.out.println(depth + " " + appTime + " " + late*4);
+        
     	boardSize = b.getSize()*b.getSize();
         int max = - boardSize-1;
         Move best = null;
         
         int alfa = - boardSize;
         int beta = boardSize;
+        int newHeur;
+        int safe=0;
     	PriorityQueue<HeuristicMove> movesQueue = new PriorityQueue<HeuristicMove>(moves.size(), new HeuristicMoveComparator());
         for (Move move : moves) {
         	b.doMove(move);
-        	movesQueue.add(new HeuristicMove(-heuristic(b,getColor()), move));
+        	newHeur = heuristic(b,getColor());
+        	/*if(heur>newHeur)
+        		safe++;
+            SnortMove m = (SnortMove) move;
+        	if(b.getState(m.getX(), m.getY()) == getColor())*/
+        	if (newHeur - heur != -1)
+        		movesQueue.add(new HeuristicMove(-newHeur, move));
         	b.undoMove(move);
         }
+        if(!movesQueue.isEmpty())
+        	best = movesQueue.peek().move;
+        else
+        	best = moves.get(0);
         Move move;
         Color opColor = getOpponent(getColor());
-        while(!movesQueue.isEmpty()) {
-        	move = movesQueue.poll().move;
-        	b.doMove(move);
-        	alfa = opAlphabeta(b,depth - 1, alfa, beta, opColor);
-        	if (max<alfa) {
-        		max = alfa;
-        		best = move;
-        	}
-
-        	b.undoMove(move);
+        try {
+        	if(depth>1)
+		        while(!movesQueue.isEmpty()) {
+		        	move = movesQueue.poll().move;
+		        	b.doMove(move);
+		        	alfa = opAlphabeta(b,depth - 1, alfa, beta, opColor);
+		        	if (max<alfa) {
+		        		max = alfa;
+		        		best = move;
+		        	}
+		
+		        	b.undoMove(move);
+		        }
+	    	long end_time = System.currentTimeMillis();
+	    	double difference = (end_time - startTime);
+	    	System.out.println(difference);
+	    	late=0;
+        } catch (NoMoreTimeException e) {
+        	late=movesQueue.size()/(double)moves.size();
+        	System.out.println("I was too slow... I left " + movesQueue.size() + "/" + moves.size() + " nodes alone :( ");
         }
         return best;
     }
 
-    public int alphabeta(Board b, int depth, int alfa, int beta, Color color){
-    	//if(true) return 0;
-        if (b.getMovesFor(color).size() == 0) {
-        	if (color == getColor())
-        		return -boardSize;
-        	else
-        		return boardSize;
-        }
-        else if (depth == 0) {
-            return heuristic(b, getOpponent(color));
-        }
-        else if (color == getColor()) {
-            for (Move move: b.getMovesFor(color)) {
-                b.doMove(move);
-                Move last_move = move;
-                alfa = Math.max(alfa, alphabeta(b, depth - 1, alfa, beta, getOpponent(color)));
-                b.undoMove(move);
-                if (beta <= alfa){
-                    break;
-                }
-            }
-            return alfa;
-        }
-        else {
-            for (Move move: b.getMovesFor(color)) {
-                b.doMove(move);
-                Move last_move = move;
-                beta = Math.min(beta, alphabeta(b, depth - 1, alfa, beta, getOpponent(color)));
-                b.undoMove(move);
-                if (beta <= alfa){
-                    break;
-                }
-            }
-            return beta;
-        }
-    }
-
-    public int opAlphabeta(Board b, int depth, int alfa, int beta, Color color){
+    public int opAlphabeta(Board b, int depth, int alfa, int beta, Color color) throws NoMoreTimeException{
+    	if (getTime() <= System.currentTimeMillis() - startTime + 100)
+    		throw new NoMoreTimeException();
         if (b.getMovesFor(color).size() == 0) {
         	if (color == getColor())
         		return -boardSize;
@@ -123,7 +117,7 @@ public class SnortSnort extends Player {
         	PriorityQueue<HeuristicMove> movesQueue = new PriorityQueue<HeuristicMove>(moves.size(), new HeuristicMoveComparator());
             for (Move move: moves) {
             	b.doMove(move);
-            	movesQueue.add(new HeuristicMove(heuristic(b,color), move));
+            	movesQueue.add(new HeuristicMove(-heuristic(b,color), move));
             	b.undoMove(move);
             }
             Move move;
@@ -144,7 +138,7 @@ public class SnortSnort extends Player {
         	PriorityQueue<HeuristicMove> movesQueue = new PriorityQueue<HeuristicMove>(moves.size(), new HeuristicMoveComparator());
             for (Move move: moves) {
             	b.doMove(move);
-            	movesQueue.add(new HeuristicMove(heuristic(b,getOpponent(color)), move));
+            	movesQueue.add(new HeuristicMove(heuristic(b,color), move));
             	b.undoMove(move);
             }
             Move move;
